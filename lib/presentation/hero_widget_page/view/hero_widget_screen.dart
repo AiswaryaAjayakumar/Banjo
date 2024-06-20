c557e46f688d0057a071014352705e63318e9e39
AiswaryaAjayakumar <aiswaryaajayakumar003@gmail.com>
2024-06-20 15:53:59 +0530

    updated

--- lib/presentation/hero_widget_page/view/hero_widget_screen.dart
+++ lib/presentation/hero_widget_page/view/hero_widget_screen.dart
@@ -6,6 +6,7 @@ import 'package:marquee/marquee.dart';
 import 'package:music_app/core/constants/color.dart';
 import 'package:music_app/presentation/home_page/controller/song_data_controller.dart';
 import 'package:music_app/presentation/home_page/controller/song_player_controller.dart';
+import 'package:music_app/presentation/home_page/view/song_page.dart';
 
 class HeroWidgetScreen extends StatefulWidget {
   const HeroWidgetScreen({
@@ -25,89 +26,98 @@ class _HeroWidgetScreenState extends State<HeroWidgetScreen> {
   Widget build(BuildContext context) {
     return Hero(
       tag: "myImage",
-      child: Container(
-        height: MediaQuery.of(context).size.height / 12,
-        width: MediaQuery.of(context).size.width / 1.1,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(5),
-          color: ColorConstants.containerOrange,
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  Container(
-                    height: 50,
-                    width: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      image: DecorationImage(
-                        image: AssetImage("assets/images/Iphone.jpeg"),
-                        fit: BoxFit.cover,
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SongPageScreen(),
+              ));
+        },
+        child: Container(
+          height: MediaQuery.of(context).size.height / 12,
+          width: MediaQuery.of(context).size.width / 1.1,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: Color.fromARGB(255, 3, 227, 243),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                          image: AssetImage("assets/images/Iphone.jpeg"),
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(width: 18),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.5,
-                        child: Text(
-                          songPlayerController.songTitle.value,
-                          overflow: TextOverflow.fade,
-                          maxLines: 1,
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
+                    SizedBox(width: 18),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.5,
+                          child: Text(
+                            songPlayerController.songTitle.value,
+                            overflow: TextOverflow.fade,
+                            maxLines: 1,
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                            ),
                           ),
                         ),
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.5,
-                        child: Text(
-                          songPlayerController.songArtist.value,
-                          //overflow: TextOverflow.fade,
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: ColorConstants.customGrey1,
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.5,
+                          child: Text(
+                            songPlayerController.songArtist.value,
+                            //overflow: TextOverflow.fade,
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: ColorConstants.customGrey1,
+                            ),
                           ),
                         ),
+                      ],
+                    ),
+                  ],
+                ),
+                Obx(
+                  () => InkWell(
+                    onTap: () {
+                      if (songPlayerController.isPlaying.value) {
+                        songPlayerController.pausePlaying();
+                        // aniController.stop();  // Uncomment if using animation
+                      } else {
+                        songPlayerController.resumePlaying();
+                        // aniController.repeat();  // Uncomment if using animation
+                      }
+                    },
+                    child: CircleAvatar(
+                      radius: 20,
+                      backgroundColor: ColorConstants.customWhite1,
+                      child: Icon(
+                        songPlayerController.isPlaying.value
+                            ? Icons.pause
+                            : Icons.play_arrow,
+                        color: ColorConstants.customGrey1,
                       ),
-                    ],
-                  ),
-                ],
-              ),
-              Obx(
-                () => InkWell(
-                  onTap: () {
-                    if (songPlayerController.isPlaying.value) {
-                      songPlayerController.pausePlaying();
-                      // aniController.stop();  // Uncomment if using animation
-                    } else {
-                      songPlayerController.resumePlaying();
-                      // aniController.repeat();  // Uncomment if using animation
-                    }
-                  },
-                  child: CircleAvatar(
-                    radius: 20,
-                    backgroundColor: ColorConstants.customWhite1,
-                    child: Icon(
-                      songPlayerController.isPlaying.value
-                          ? Icons.pause
-                          : Icons.play_arrow,
-                      color: ColorConstants.customGrey1,
                     ),
                   ),
                 ),
-              ),
-              // IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
-            ],
+                // IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+              ],
+            ),
           ),
         ),
       ),
